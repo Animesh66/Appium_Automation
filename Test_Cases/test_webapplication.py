39daf27f34ac0f29f3859f903065d35e1b716253
Animesh <er.animesh6@hotmail.com>
2021-07-28 17:43:37 +0530

    Appium code

--- Test_Cases/test_webapplication.py
+++ Test_Cases/test_webapplication.py
@@ -1,1 +1,17 @@
+import time
+
 from appium import webdriver
+
+desired_cap = dict(
+
+    deviceName='Samsung',
+    platformName='Android',
+    browserName='Chrome'
+)
+
+driver = webdriver.Remote('http://127.0.0.1:4273/wd/hub', desired_cap)
+driver.get("https://google.com")
+driver.title
+time.sleep(2)
+driver.quit()
+
