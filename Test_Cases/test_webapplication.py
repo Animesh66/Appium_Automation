555386f9bdf6557ebe4d4f4a47772e27789c1583
Animesh <er.animesh6@hotmail.com>
2021-07-30 19:08:58 +0530

    Appium code. first test case complete.

--- Test_Cases/test_webapplication.py
+++ Test_Cases/test_webapplication.py
@@ -14,7 +14,6 @@ driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
 driver.get("https://www.facebook.com/")
 page_title = driver.title
 print(page_title)
-email_textbox = driver.find_element_by_id("email")
-email_textbox.send_keys("animesh@abc.com")
+driver.find_element_by_xpath("//input[@id='m_login_email']").send_keys("animesh@abc.com")
 time.sleep(3)
 driver.quit()
