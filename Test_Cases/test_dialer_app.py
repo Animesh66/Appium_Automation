d794c3b5638b83e7dd11f805da9c8eaf3d0ec0a2
Animesh <er.animesh6@hotmail.com>
2021-07-31 10:45:44 +0530

    Appium code. Second test case complete.

--- Test_Cases/test_dialer_app.py
+++ Test_Cases/test_dialer_app.py
@@ -6,14 +6,11 @@ desired_cap = dict(
     deviceName='Galaxy Note 10',
     platformName='Android',
     platformVersion='11',
-    browserName='Chrome',
+    appPackage='com.samsung.android.dialer',
+    appActivity='.DialtactsActivity',
     automationName='UiAutomator2'
 )
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
-driver.get("https://www.facebook.com/")
-page_title = driver.title
-print(page_title)
-driver.find_element_by_xpath("//input[@id='m_login_email']").send_keys("animesh@abc.com")
-time.sleep(3)
+time.sleep(5)
 driver.quit()
