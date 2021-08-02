39fe076c4ff52a59f48499cac63f9d98caecfef8
Animesh <er.animesh6@hotmail.com>
2021-08-02 17:39:56 +0530

    Appium code. Second test case complete.

---
+++ Test_Cases/test_adding_contact.py
@@ -0,0 +1,16 @@
+import time
+from appium import webdriver
+
+
+desired_cap = dict(
+    deviceName='Galaxy Note 10',
+    platformName='Android',
+    platformVersion='11',
+    appPackage='com.samsung.android.app.contacts',
+    appActivity='com.samsung.android.contacts.contactslist.PeopleActivity',
+    automationName='UiAutomator2'
+)
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
+time.sleep(5)
+driver.quit()
