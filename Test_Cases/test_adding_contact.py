9118d5aef176c73dc7a9cf7ca54389e18a6d9ab3
Animesh <er.animesh6@hotmail.com>
2021-08-02 20:53:12 +0530

    Appium code. Second test case complete.

--- Test_Cases/test_adding_contact.py
+++ Test_Cases/test_adding_contact.py
@@ -12,5 +12,13 @@ desired_cap = dict(
 )
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
+driver.implicitly_wait(10)
+driver.find_element_by_xpath("//android.widget.Button[@content-desc='Create contact']")
+driver.find_element_by_id("com.samsung.android.app.contacts:id/check").click()
+driver.find_element_by_id("android:id/button2").click()
+driver.find_element_by_id("com.samsung.android.app.contacts:id/nameEdit").send_keys("John")
+driver.find_element_by_id("com.samsung.android.app.contacts:id/titleLayout").send_keys("7979878971")
+driver.hide_keyboard()
+driver.find_element_by_id("//android.widget.Button[@content-desc='Save']/android.view.ViewGroup/android.widget.TextView").click()
 time.sleep(5)
 driver.quit()
