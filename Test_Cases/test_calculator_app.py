c8b3ab40f9b54ae82309be79a5c7d7dc08096fa4
Animesh <er.animesh6@hotmail.com>
2021-08-02 22:24:18 +0530

    Appium code. forth test case complete.

---
+++ Test_Cases/test_calculator_app.py
@@ -0,0 +1,12 @@
+from appium import webdriver
+
+desired_cap = {
+    'deviceName': 'Galaxy Note 10',
+    'platformName': 'Android',
+    'platformVersion': '11',
+    'appPackage': 'com.samsung.android.app.contacts',
+    'appActivity': 'com.samsung.android.contacts.contactslist.PeopleActivity',
+    'automationName': 'UiAutomator2'
+}
+
+
