7cb5e189b5cd84f40c5ed68685f3cadb3f6f63df
Animesh <er.animesh6@hotmail.com>
2021-08-03 09:29:21 +0530

    Appium code. forth test case complete.

---
+++ Test_Cases/test_install_APK.py
@@ -0,0 +1,16 @@
+import time
+
+from appium import webdriver
+
+desired_cap = dict(
+    deviceName='Galaxy Note 10',
+    platformName='Android',
+    platformVersion='11',
+    app='/Users/animeshmukherjee/PycharmProjects/pythonProject/Appium_Automation/Android_App/CarWale_com.carwale.apk',
+    automationName='UiAutomator2'
+)
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
+driver.implicitly_wait(10)
+time.sleep(3)
+driver.quit()
