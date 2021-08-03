08f11d976d37bf1a4db1bdbb71cdb56d2e0875fe
Animesh <er.animesh6@hotmail.com>
2021-08-03 12:27:26 +0530

    Appium code. fifth test case complete.

---
+++ Test_Cases/test_demo_simple.py
@@ -0,0 +1,26 @@
+import time
+
+from appium import webdriver
+
+desired_cap = dict(
+    deviceName='Galaxy Note 10',
+    platformName='Android',
+    platformVersion='11',
+    app='/Users/animeshmukherjee/PycharmProjects/pythonProject/Appium_Automation/Android_App/Simple DEMO_org.simple.clinic.staging.apk',
+    automationName='UiAutomator2'
+)
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
+driver.implicitly_wait(10)
+driver.find_element_by_id("org.simple.clinic.staging:id/nextButton").click()
+driver.find_element_by_id("org.simple.clinic.staging:id/getStartedButton").click()
+driver.find_element_by_xpath("//android.widget.RadioButton[@text='India']").click()
+driver.find_element_by_id("org.simple.clinic.staging:id/nextButton").click()
+driver.find_element_by_id("org.simple.clinic.staging:id/phoneNumberEditText").send_keys("8123456789")
+driver.find_element_by_id("org.simple.clinic.staging:id/nextButton").click()
+driver.find_element_by_id("org.simple.clinic.staging:id/fullNameEditText").send_keys("Mamam")
+driver.find_element_by_id("org.simple.clinic.staging:id/nextButton").click()
+driver.find_element_by_id("org.simple.clinic.staging:id/pinEditText").send_keys("1234")
+driver.find_element_by_id("org.simple.clinic.staging:id/confirmPinEditText").send_keys("1234")
+time.sleep(3)
+driver.quit()
