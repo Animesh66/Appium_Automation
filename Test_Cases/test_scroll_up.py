b6ac22a12d81de7be194294d48895046851c1d36
Animesh <er.animesh6@hotmail.com>
2021-08-04 20:39:48 +0530

    Appium code. sixth test case complete.

---
+++ Test_Cases/test_scroll_up.py
@@ -0,0 +1,20 @@
+from appium import webdriver
+
+desired_cap = dict(
+    deviceName='Galaxy Note 10',
+    platformName='Android',
+    platformVersion='11',
+    appPackage='com.samsung.android.app.contacts',
+    appActivity='com.samsung.android.contacts.contactslist.PeopleActivity',
+    automationName='UiAutomator2'
+)
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
+driver.swipe(510, 600, 510, 200, 1000)
+driver.swipe(510, 600, 510, 200, 1000)
+driver.swipe(510, 600, 510, 200, 1000)
+driver.swipe(510, 600, 510, 200, 1000)
+driver.swipe(510, 200, 510, 600, 1000)
+driver.swipe(510, 200, 510, 600, 1000)
+driver.swipe(510, 200, 510, 600, 1000)
+driver.swipe(510, 200, 510, 600, 1000)
