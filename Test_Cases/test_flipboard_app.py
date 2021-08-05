2490976ad88f943b55aa4f0d56004e9814e815e9
Animesh <er.animesh6@hotmail.com>
2021-08-05 22:54:23 +0530

    Appium code. flipboard application test case complete.

--- Test_Cases/test_flipboard_app.py
+++ Test_Cases/test_flipboard_app.py
@@ -6,9 +6,11 @@ desired_cap = dict(
     platformName='Android',
     platformVersion='11',
     appPackage='flipboard.app',
-    appActivity='flipboard.activities.FirstLaunchCoverActivity',
+    appActivity='flipboard.activities.AccountLoginActivity',
     automationName='UiAutomator2'
 )
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
 driver.implicitly_wait(10)
+driver.find_element_by_id("flipboard.app:id/account_login_email_button").click()
+driver.find_element_by_xpath("//android.widget.TextView[@text='animesh5678@gmail.com']").click()
