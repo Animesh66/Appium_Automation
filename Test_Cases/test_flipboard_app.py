50297565752fcea6160f9ad7f497e86177c887ca
Animesh <er.animesh6@hotmail.com>
2021-08-07 12:28:18 +0530

    Appium code. flipboard application test case complete.

--- Test_Cases/test_flipboard_app.py
+++ Test_Cases/test_flipboard_app.py
@@ -14,3 +14,8 @@ driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
 driver.implicitly_wait(10)
 driver.find_element_by_id("flipboard.app:id/account_login_email_button").click()
 driver.find_element_by_xpath("//android.widget.TextView[@text='animesh5678@gmail.com']").click()
+driver.find_element_by_id("flipboard.app:id/account_login_password").send_keys("Maximum9093@")
+driver.find_element_by_id("flipboard.app:id/icon_button_text").click()
+if driver.find_element_by_id("android:id/button1"):
+    driver.find_element_by_id("android:id/button1").click()
+driver.find_element_by_id("com.google.android.gms:id/credential_save_reject").click()
