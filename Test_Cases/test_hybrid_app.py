ccfb4826dcebdfbedcea5226228cf7b8ec93e57d
Animesh <er.animesh6@hotmail.com>
2021-08-11 23:16:03 +0530

    Appium code. flipboard application test case complete.

--- Test_Cases/test_hybrid_app.py
+++ Test_Cases/test_hybrid_app.py
@@ -1,3 +1,5 @@
+import time
+
 from appium import webdriver
 
 desired_cap = dict(
@@ -12,6 +14,9 @@ desired_cap = dict(
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
 driver.implicitly_wait(10)
 driver.get("https://www.google.com")
+time.sleep(3)
 contexts = driver.contexts  # get the context of the hybrid app
 for context in contexts:
     print(context)
+driver.switch_to.context("WEBVIEW_chrome")
+driver.find_element_by_id("com.android.chrome:id/search_box_text").send_keys("Hello Appium")
