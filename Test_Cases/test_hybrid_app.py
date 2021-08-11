8a2ff0861d7a91488c004424decfc13244e9de47
Animesh <er.animesh6@hotmail.com>
2021-08-11 23:05:13 +0530

    Appium code. flipboard application test case complete.

--- Test_Cases/test_hybrid_app.py
+++ Test_Cases/test_hybrid_app.py
@@ -4,10 +4,12 @@ desired_cap = dict(
     deviceName='Galaxy Note 10',
     platformName='Android',
     platformVersion='11',
-    appPackage='com.samsung.android.app.spage',
-    appActivity='.ui.MainActivity',
+    appPackage='com.android.chrome',
+    appActivity='org.chromium.chrome.browser.ChromeTabbedActivity',
     automationName='UiAutomator2'
 )
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
 driver.implicitly_wait(10)
+driver.get("https://www.google.com")
+
