fa1969d4b6c21ca0d4eb53eb54d41fc4f0db638b
Animesh <er.animesh6@hotmail.com>
2021-08-15 19:49:44 +0530

    Appium code. automate goibibo application test case complete.

---
+++ Test_Cases/test_grid_execution.py
@@ -0,0 +1,45 @@
+import time
+import pytest
+from appium import webdriver
+
+
+@pytest.fixture(params=['note_10', 'nexus_7'], scope="function")
+def appium_driver(request):
+
+    if request.param == 'note_10':
+        desired_cap = dict(
+            deviceName='Galaxy Note 10',
+            platformName='Android',
+            platformVersion='11',
+            udid='R58M86QW34P',
+            appPackage='com.instagram.android',
+            appActivity='com.instagram.mainactivity.MainActivity',
+            automationName='UiAutomator2',
+            noReset=False
+        )
+        driver = webdriver.Remote('http://127.0.0.1:4444/wd/hub', desired_cap)
+
+    if request.param == 'nexus_7':
+        desired_cap = dict(
+            deviceName='Nexus 7',
+            platformName='Android',
+            platformVersion='8.1',
+            udid='emulator-5554',
+            appPackage='com.instagram.android',
+            appActivity='com.instagram.mainactivity.MainActivity',
+            automationName='UiAutomator2',
+            noReset=False
+        )
+        driver = webdriver.Remote('http://127.0.0.1:4444/wd/hub', desired_cap)
+
+    driver.implicitly_wait(10)
+    yield driver
+    time.sleep(3)
+    driver.quit()
+
+
+def test_instagram(appium_driver):
+    driver = appium_driver
+    driver.find_element_by_id("com.google.android.gms:id/cancel").click()
+
+
