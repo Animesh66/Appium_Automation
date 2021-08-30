ccf6f557ce13d6ec65ad120afa7b60a1f37d42e8
Animesh <er.animesh6@hotmail.com>
2021-08-30 21:17:17 +0530

    Appium code. automate goibibo application test case complete.

---
+++ Test_Cases/test_scroll_contact.py
@@ -0,0 +1,21 @@
+import time
+from appium import webdriver
+
+desired_cap = dict(
+    deviceName='Samsung Note 10',
+    platformName='Android',
+    platformVersion='11',
+    appPackage='com.samsung.android.app.contacts',
+    appActivity='com.samsung.android.contacts.contactslist.PeopleActivity',
+    automationName='UiAutomator2'
+)
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_cap)
+driver.implicitly_wait(10)
+element = driver.find_elements_by_accessibility_id("Dadu")
+driver.execute_script("arguments[0].ScrollIntoView", element)
+element.click()
+time.sleep(5)
+driver.quit()
+
+
